2fd8c5f8c797e820e9c02cc066aa4b0433730aa7
nyainanathan <nyaina.nathan.1@gmail.com>
2025-08-07 11:10:16 +0300

    feat: get every match of a fighter method implementation

--- src/main/java/Ligue.java
+++ src/main/java/Ligue.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import lombok.*;
 
@@ -8,4 +9,12 @@ public class Ligue {
     private String nom;
     private List<Combattant> combattants;
     private List<Match> matches;
+
+    public List<Match> getMatchDeCombattant(Combattant combattant){
+        List<Match> matches = this.getMatches();
+        return matches.stream()
+                .filter(match -> match.getCombattants().contains(combattant))
+                .toList();
+    }
+
 }
